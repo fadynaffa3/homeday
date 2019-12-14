16dca45e15808d71ff6297e6aceb81235a2490ff
Fady Naffa <fadynaffa3@gmail.com>
2019-12-14 16:07:06 +0200

    submiting score

--- app/controllers/nps_feedbacks_controller.rb
+++ app/controllers/nps_feedbacks_controller.rb
@@ -4,4 +4,14 @@ class NpsFeedbacksController < ApplicationController
   def index
     render_resources(@nps_feedbacks)
   end
+
+  def update
+    @nps_feedback.update_attributes(nps_feedback_params)
+  end
+
+  private
+
+  def nps_feedback_params
+    params.require(:nps_feedback).permit(:score)
+  end
 end
--- app/models/nps_feedback.rb
+++ app/models/nps_feedback.rb
@@ -3,6 +3,10 @@ class NpsFeedback < ApplicationRecord
   belongs_to :object, polymorphic: true
 
   validates :touchpoint, presence: true
+  validates_numericality_of :score, :only_integer => true, :allow_nil => true,
+    :greater_than_or_equal_to => 1,
+    :less_than_or_equal_to => 10,
+    :message => "can only be whole number between 1 and 10."
 
   after_create :send_email
 
